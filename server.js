4176e1278434f4c2ddc08499c8661665b1d7a218
Naveed Ali <advmiannaveedali.com>
2024-08-07 18:28:43 +0500

    connect db

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const errorHandler = require("./middleware/errorHandler");
+const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
 
+connectDb();
  const app = express();
 
  const port = process.env.PORT || 2000;
 
-app.use(express.json());
- app.use("/api/contacts", require("./routes/contactRoutes"))
+ app.use(express.json());
+ app.use("/api/contacts", require("./routes/contactRoutes"));
+ app.use(errorHandler);
 
  app.listen(port, ()=> {
     console.log(`Server is running on port ${port}`);
