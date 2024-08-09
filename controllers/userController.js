5f135f351b23dfef07696b87ad0ab72f6d290fc8
Naveed Ali <advmiannaveedali@gmail.com>
2024-08-09 10:46:35 +0500

    create user controller

---
+++ controllers/userController.js
@@ -0,0 +1,48 @@
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+
+// Register users
+//routes GET /api/register/users
+const registerUser = asyncHandler(async (req, res) => {
+    const {username,email,password} = req.body;
+  if (!username || !email || !password) {
+      res.status(400);
+      throw new Error("All fields are mandatory");
+  }
+  const currentUser = await User.findOne({email});
+  if (currentUser) {
+    res.status(400);
+      throw new Error("User Already existed");
+  };
+  //hashed passwrod
+  const hashedPassword = await bcrypt.hash(password, 10)
+  console.log("Hashed Password",hashedPassword);
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+  console.log(`User created ${user}`);
+  if (user){
+    res.status(201).json({_id: user.id, email: user.email})
+  } else {
+    throw new Error("User data not valid")
+  }
+  res.json({message:"Register the user"});
+
+});
+
+// Login users
+//routes GET /api//users/login
+const loginUser = asyncHandler(async (req, res) => {
+    res.json({message:"Login the user"});
+});
+
+//  user info
+//routes GET /api//users/login
+const currentUser = asyncHandler(async (req, res) => {
+    res.json({message:"Current user info"});
+});
+
+module.exports = {registerUser,loginUser,currentUser};
