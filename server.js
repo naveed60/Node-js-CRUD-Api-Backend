081c7a5ce940da1132d81c0d6f996dabe6a0f499
Naveed Ali <advmiannaveedali@gmail.com>
2024-08-09 10:46:16 +0500

    update user route

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ connectDb();
 
  app.use(express.json());
  app.use("/api/contacts", require("./routes/contactRoutes"));
+ app.use("/api/users", require("./routes/userRoutes"));
+
  app.use(errorHandler);
 
  app.listen(port, ()=> {
