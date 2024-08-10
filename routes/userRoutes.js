2e4d9499824805cec097dedf671192a22b678fc4
Naveed Ali <advmiannaveedali@gmail.com>
2024-08-10 22:30:24 +0500

    protected user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { registerUser, loginUser, currentUser } = require("../controllers/userController");
+const validateToken = require("../middleware/validateTokenHandler");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.post("./current ", currentUser);
+router.get("/current", validateToken, currentUser); // Corrected to GET request and fixed the path
 
 module.exports = router;
