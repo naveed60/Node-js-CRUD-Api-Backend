59e6610bb0c5e44561c856f383c83ca385b2e89e
Naveed Ali <advmiannaveedali@gmail.com>
2024-08-10 22:28:50 +0500

    user authentication with jwt token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // Register users
 //routes GET /api/register/users
@@ -36,13 +37,34 @@ const registerUser = asyncHandler(async (req, res) => {
 // Login users
 //routes GET /api//users/login
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({message:"Login the user"});
+   const {email, password} = req.body;
+   if (!email || !password){
+    res.status(400);
+    throw new Error("Email and Password Required")
+   }
+   const  user = await User.findOne({email});
+   if(user && (await bcrypt.compare(password, user.password))){
+    const accessToken = jwt.sign({
+      user:{
+        username: user.username,
+        email: user.email,
+        id: user.id,
+      }
+    },process.env.ACCESS_TOKEN_SECRET,
+    {expiresIn:"15m"}
+  );
+    res.status(200).json({accessToken})
+   }else {
+    res.status(401);
+    throw new Error("email or password not valid");
+   }
 });
 
 //  user info
 //routes GET /api//users/login
 const currentUser = asyncHandler(async (req, res) => {
-    res.json({message:"Current user info"});
+    res.json(req.user);
+    console.log("currentUser",req.user)
 });
 
 module.exports = {registerUser,loginUser,currentUser};
