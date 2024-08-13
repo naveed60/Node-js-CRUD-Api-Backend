1911b295784ba5007972452a913ae8ea753ca6ab
Naveed Ali <advmiannaveedali@gmail.com>
2024-08-13 11:07:35 +0500

    frontend added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import { ContactProvider } from "./contexts/ContactContext";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import ContactList from "./components/Contacts/ContactList";
+import ContactForm from "./components/Contacts/ContactForm";
+import ContactDetail from "./components/Contacts/ContactDetail";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <ContactProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/contacts" element={<ContactList />} />
+            <Route path="/contacts/new" element={<ContactForm />} />
+            <Route path="/contacts/:id" element={<ContactDetail />} />
+          </Routes>
+        </Router>
+      </ContactProvider>
+    </AuthProvider>
   );
 }
 
