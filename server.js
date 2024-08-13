352641eef436a128cef06da947b7e7581177ae75
Naveed Ali <advmiannaveedali@gmail.com>
2024-08-13 11:07:55 +0500

    server routes protection

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
 const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
+const cors = require("cors")
 
 connectDb();
  const app = express();
@@ -11,6 +12,12 @@ connectDb();
  app.use(express.json());
  app.use("/api/contacts", require("./routes/contactRoutes"));
  app.use("/api/users", require("./routes/userRoutes"));
+ app.get('/api/test', (req, res) => {
+   res.json({ message: 'API is working' });
+ });
+ 
+// In your backend server file
+app.use(cors());
 
  app.use(errorHandler);
 
