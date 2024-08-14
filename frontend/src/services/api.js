70dc9b3eb704ce6875bd67c7cfeab5edd7452d66
Naveed Ali <advmiannaveedali@gmail.com>
2024-08-14 11:30:26 +0500

    fix api frontend  issues

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3300/api";
+const API_BASE_URL = "http://localhost:3600/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
@@ -9,35 +9,28 @@ const api = axios.create({
     "Content-Type": "application/json",
   },
 });
-api.get('/test').then(response => console.log(response)).catch(error => console.error(error));
 
-export const registerUser = (userData) => api.post("/register", userData);
-console.log("registerUser",registerUser)
-export const loginUser = (userData) => api.post("/login", userData);
-export const getCurrentUser = (token) =>
-  api.get("/current", {
-    headers: { Authorization: `Bearer ${token}` },
-  });
+// Request interceptor to add the token to each request
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
-export const getContacts = (token) =>
-  api.get("/contacts", {
-    headers: { Authorization: `Bearer ${token}` },
-  });
-export const getContactById = (id, token) =>
-  api.get(`/contacts/${id}`, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
-export const createContact = (contactData, token) =>
-  api.post("/contacts", contactData, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
-export const updateContact = (id, contactData, token) =>
-  api.put(`/contacts/${id}`, contactData, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
-export const deleteContact = (id, token) =>
-  api.delete(`/contacts/${id}`, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
+export const registerUser = (userData) => api.post("users/register", userData);
+export const loginUser = (userData) => api.post("users/login", userData);
+export const getCurrentUser = () => api.get("/current");
+export const getContacts = () => api.get("/contacts");
+export const getContactById = (id) => api.get(`/contacts/${id}`);
+export const createContact = (contactData) => api.post("/contacts", contactData);
+export const updateContact = (id, contactData) => api.put(`/contacts/${id}`, contactData);
+export const deleteContact = (id) => api.delete(`/contacts/${id}`);
 
 export default api;
